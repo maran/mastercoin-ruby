dbc67be1777a754fb6f02c00cd4e83b164c524bf
Maran <maran.hidskes@gmail.com>
2013-10-21 15:54:54 +0200

    Implement keystripping and fix sequence

--- lib/mastercoin-ruby/util.rb
+++ lib/mastercoin-ruby/util.rb
@@ -1,5 +1,17 @@
 module Mastercoin
   class Util
+    def self.sort_keys(public_keys)
+      public_keys.sort{|x,y| x[2..3] <=> y[2..3]}
+    end
+
+    def self.strip_key(key)
+      return key[4..-1]
+    end
+
+    def self.sort_and_strip_keys(keys)
+      Util.sort_keys(keys).collect{|key| Util.strip_key(key)}
+    end
+
     def self.valid_ecdsa_point?(pub_key)
       begin
         Bitcoin::Key.new(nil, pub_key).addr
@@ -18,7 +30,7 @@ module Mastercoin
         seq -= 255
       end
 
-      return seq
+      return seq.abs
     end
   end
 end
